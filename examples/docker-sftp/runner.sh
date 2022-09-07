9354f109cf304d55a75dbe44e29b21b2dc186bc6
Adam Scanga <sandmanscanga@gmail.com>
2022-09-06 22:52:15 -0400

    Cleaned up the SFTP docker process to be more streamlined and require no user interaction other than executing the runner.sh script

--- examples/docker-sftp/runner.sh
+++ examples/docker-sftp/runner.sh
@@ -1,78 +1,86 @@
 #!/bin/bash
 
-docker-clean -a
-docker-compose up -d
+# Verify current working directory
+if [[ ! -f runner.sh ]] || [[ ! -f docker-compose.yml ]]; then
+    echo "Please change into the SFTP docker compose directory!" >&2
+    exit 1
+fi
 
-## install this .ssh/config to avoid problems
-# Host 172.69.0.*
-#    StrictHostKeyChecking no
-#    UserKnownHostsFile=/dev/null
+# Set environment variables
+export SFTP_ADDR="172.69.0.1"
+export SFTP_USER="tester"
+export SFTP_PASS=$(cat /dev/random | xxd -p | head -c 24)
 
-## this command should grab a file without a problem
-# sftp -i volume/keyring/id_rsa testing@172.69.0.2:/share/testing.dat .
+## Set top level variables
+PROJECT_ROOT=$(readlink -f .)
+SSH_KNOWN_HOSTS=$HOME/.ssh/known_hosts
+VOLUME_ROOT=$PROJECT_ROOT/volume
 
-########################################################################
-## basic usage
-# docker run -p 22:22 -d atmoz/sftp foo:pass:::upload
-########################################################################
+# Set SSH hostkey paths
+HOSTKEYS_ROOT=$VOLUME_ROOT/hostkeys
+ED25519_KEYS_PATH=$HOSTKEYS_ROOT/ssh_host_ed25519_key
+RSA_KEYS_PATH=$HOSTKEYS_ROOT/ssh_host_rsa_key
 
-########################################################################
-## sharing directory from host
-# docker run \
-#     -v <host-dir>/upload:/home/foo/upload \
-#     -p 2222:22 -d atmoz/sftp \
-#     foo:pass:1001
-########################################################################
-## using docker compose
-# sftp:
-#     image: atmoz/sftp
-#     volumes:
-#         - <host-dir>/upload:/home/foo/upload
-#     ports:
-#         - "2222:22"
-#     command: foo:pass:1001
-########################################################################
+# Set SSH authentication keyring paths
+KEYRING_ROOT=$VOLUME_ROOT/keyring
+ID_RSA_KEYS_PATH=$KEYRING_ROOT/id_rsa
+LOCAL_RSA_KEYS_PATH=$PROJECT_ROOT/sftp_key
 
-########################################################################
-## storing users in config
-# docker run \
-#     -v <host-dir>/users.conf:/etc/sftp/users.conf:ro \
-#     -v mySftpVolume:/home \
-#     -p 2222:22 -d atmoz/sftp
-#
-## foo:123:1001:100
-## bar:abc:1002:100
-## baz:xyz:1003:100
-########################################################################
+# Set share volume and testfile paths
+TESTFILE_NAME=testing.dat
+SHARE_ROOT=$VOLUME_ROOT/share
+TESTFILE_OUT_PATH=$PROJECT_ROOT/$TESTFILE_NAME
+TESTFILE_IN_PATH=$SHARE_ROOT/$TESTFILE_NAME
 
-########################################################################
-## encrypted password
-# docker run \
-#     -v <host-dir>/share:/home/foo/share \
-#     -p 2222:22 -d atmoz/sftp \
-#     'foo:$1$0G2g0GSt$ewU0t6GXG15.0hWoOX8X9.:e:1001'
-########################################################################
+function clean_services {
+    # Cleanup local volume and docker data
+    rm -f $LOCAL_RSA_KEYS_PATH
+    rm -f $TESTFILE_OUT_PATH
+    docker-clean -a
+    if [ $(whoami) != 'root' ]; then
+        sudo rm -rf $VOLUME_ROOT
+    else
+        rm -rf $VOLUME_ROOT
+    fi
+}
 
-########################################################################
-## logging using ssh keys
-# docker run \
-#     -v <host-dir>/id_rsa.pub:/home/foo/.ssh/keys/id_rsa.pub:ro \
-#     -v <host-dir>/id_other.pub:/home/foo/.ssh/keys/id_other.pub:ro \
-#     -v <host-dir>/share:/home/foo/share \
-#     -p 2222:22 -d atmoz/sftp \
-#     foo::1001
-########################################################################
+function start_services {
+    # Create SSH keys, testing file, and start docker containers
+    mkdir -p $VOLUME_ROOT/{hostkeys,keyring,share}
+    ssh-keygen -t ed25519 -f $ED25519_KEYS_PATH </dev/null
+    ssh-keygen -t rsa -b 4096 -f $RSA_KEYS_PATH </dev/null
+    ssh-keygen -t rsa -b 4096 -f $ID_RSA_KEYS_PATH </dev/null
+    echo 'This is a test!' >$TESTFILE_IN_PATH
+    cp $ID_RSA_KEYS_PATH $LOCAL_RSA_KEYS_PATH
+    chmod 600 $LOCAL_RSA_KEYS_PATH
+    docker-compose up -d
+    sleep 3
+}
 
-########################################################################
-## providing your own ssh host key (recommended)
-# docker run \
-#     -v <host-dir>/ssh_host_ed25519_key:/etc/ssh/ssh_host_ed25519_key \
-#     -v <host-dir>/ssh_host_rsa_key:/etc/ssh/ssh_host_rsa_key \
-#     -v <host-dir>/share:/home/foo/share \
-#     -p 2222:22 -d atmoz/sftp \
-#     foo::1001
-#
-## generate keys with these commands
-# ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
-# ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
-########################################################################
+function get_sftp_testfile {
+    # Retrieve the testfile via SFTP
+    cp $SSH_KNOWN_HOSTS ${SSH_KNOWN_HOSTS}.tmp
+    ssh-keyscan -t rsa $SFTP_ADDR >$SSH_KNOWN_HOSTS
+    sftp -i $LOCAL_RSA_KEYS_PATH \
+        ${SFTP_USER}@${SFTP_ADDR}:/share/$TESTFILE_NAME \
+        $TESTFILE_OUT_PATH
+    mv ${SSH_KNOWN_HOSTS}.tmp $SSH_KNOWN_HOSTS
+}
+
+function execute_main {
+    if [ "$1" == "-v" ]; then
+        clean_services
+        start_services
+        get_sftp_testfile
+        cat $TESTFILE_OUT_PATH
+        clean_services
+    else
+        clean_services >/dev/null 2>/dev/null
+        start_services >/dev/null 2>/dev/null
+        get_sftp_testfile >/dev/null 2>/dev/null
+        cat $TESTFILE_OUT_PATH 2>/dev/null
+        clean_services >/dev/null 2>/dev/null
+    fi
+}
+
+execute_main $1
--- examples/docker-sftp/scripts/gen_rand_sftp_pwd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export SFTP_RANDOM_PASSWORD=$(cat /proc/sys/kernel/random/uuid| md5sum | head -c 20)
